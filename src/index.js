fccdc117e3bcfccc4e7eeb4bccf113893266b4e8
Mateusz Sikora <mateusz.sikora@nokia.com>
2017-10-31 20:12:05 +0100

    linter

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ohm from 'ohm-js';
 export default function (rawGrammar) {
   const grammar = ohm.grammar(rawGrammar);
   return `module.exports = require('ohm-js').makeRecipe(${grammar.toRecipe()});`;
-};
+}
