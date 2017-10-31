e32c990cbf089b2091c612c92fb2f1d5639c6bf7
Mateusz Sikora <mateusz.sikora@nokia.com>
2017-10-31 20:35:03 +0100

    make loader cacheable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import ohm from 'ohm-js';
 
 export default function (rawGrammar) {
+  if (this.cacheable) {
+    this.cacheable();
+  }
+
   const grammar = ohm.grammar(rawGrammar);
   return `module.exports = require('ohm-js').makeRecipe(${grammar.toRecipe()});`;
 }
